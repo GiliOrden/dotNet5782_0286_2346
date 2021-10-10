21ac25540955aa75f93432a355b0f106183db351
רבקה אדלר <adlerrivka66@gmail.com>
2021-10-11 00:00:10 +0300

    initial code+Welcome functions

---
+++ dotNet5782_0286_2346/Targil0/Program2346.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod();
+
+            Console.ReadKey();
+        }
+
+        private static void NewMethod()
+        {
+            Console.Write("Enter your name: ");
+            string Welcome2346 = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", Welcome2346);
+        }
+        static partial void Welcome0286();
+        
+    }
+}
