ba260c9930364e100940f08ee93344c01e330101
galiaorden@outlook.com <galiaorden@outlook.com>
2021-12-06 21:38:08 +0200

    update functions

--- dotNet5782_0286_2346/ConsoleUI_BL/ProgramBL.cs
+++ dotNet5782_0286_2346/ConsoleUI_BL/ProgramBL.cs
@@ -179,6 +179,9 @@ namespace ConsoleUI_BL
             Console.WriteLine("Press 3 to update customer data");
             Console.WriteLine("Press 4 to send drone to charge");
             Console.WriteLine("Press 5 to release drone from charging");
+
+            Console.WriteLine("Press 7 to collect parcel by drone");
+            Console.WriteLine("Press 8 to supply parcel by drone");
             int.TryParse(Console.ReadLine(), out userChoise);
             update = (UpdateOptions)userChoise;
             switch (update)
@@ -189,7 +192,7 @@ namespace ConsoleUI_BL
                     int.TryParse(Console.ReadLine(), out id2);
                     dl.AssignParcelToDrone(id1, id2);
                     break;
-                case UpdateOptions.CollectParcelByDrone:
+                case UpdateOptions.UpdateStationData:
                     Console.WriteLine("Please enter the parcel ID");
                     int.TryParse(Console.ReadLine(), out id1);
                     dl.CollectParcelByDrone(id1);
@@ -200,16 +203,32 @@ namespace ConsoleUI_BL
                     name = Console.ReadLine();
                     phone= Console.ReadLine();
                     bl.UpdateCustomer(id1, name, phone);
+                    Console.WriteLine("Succeeded");
                     break;
                 case UpdateOptions.SendDroneToCharge:
                     Console.WriteLine("Please enter the drone ID ");
                     int.TryParse(Console.ReadLine(), out id1);
                     bl.SendDroneToCharge(id1);
+                    Console.WriteLine("Succeeded");
                     break;
                 case UpdateOptions.ReleaseDroneFromCharge:
                     Console.WriteLine("Please enter the drone ID");
                     int.TryParse(Console.ReadLine(), out id1);
                     dl.ReleaseDroneFromCharge(id1);
+                    break;
+                case UpdateOptions.AssignParcelToDrone:
+                    break;
+                case UpdateOptions.CollectParcelByDrone:
+                    Console.WriteLine("Please enter the drone ID");
+                    int.TryParse(Console.ReadLine(), out id1);
+                    bl.CollectingParcelByDrones(id1);
+                    Console.WriteLine("Succeeded");
+                    break;
+                case UpdateOptions.SupplyParcelByDrone:
+                    Console.WriteLine("Please enter the drone ID");
+                    int.TryParse(Console.ReadLine(), out id1);
+                    bl.SupplyDeliveryToCustomer(id1);
+                    Console.WriteLine("Succeeded");
                     break;
                 default:
                     break;
