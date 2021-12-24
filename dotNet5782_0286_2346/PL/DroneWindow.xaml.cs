02b1ce353bffa2a95192d0c867500b1f8726c2de
rivkaadler1 <adlerrivka66@gmail.com>
2021-12-24 14:44:52 +0200

    start of the update window

--- dotNet5782_0286_2346/PL/DroneWindow.xaml.cs
+++ dotNet5782_0286_2346/PL/DroneWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using IBL.BO;
 namespace PL
 {
     /// <summary>
@@ -22,13 +22,20 @@ namespace PL
         public DroneWindow(ref IBL.IBL bl)//first constructor
         {
             InitializeComponent();
-
+            updateButton.Visibility = Visibility.Collapsed;
         }
 
         public DroneWindow(ref IBL.BO.DroneForList drone)//second constructor
         {
             InitializeComponent();
-            
+            addButton.Visibility = Visibility.Collapsed;
+            idTextBox.IsEnabled = false;
+            MaxWeightComboBox.IsEnabled = false;
+            droneWindowGrid.DataContext = drone;
+            DroneStatusComboBox.DataContext = drone.DroneStatus;
+            MaxWeightComboBox.DataContext = drone.MaxWeight;
+            longitudeTextBox.DataContext = drone.Location.Longitude;
+            latitudeTextBox.DataContext = drone.Location.Latitude;
         }
 
         private void cancelButton_Click(object sender, RoutedEventArgs e)
@@ -37,6 +44,11 @@ namespace PL
         }
 
         private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
