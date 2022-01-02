a63c524d17d3bcfe815a41250607519e74f15514
rivkaadler1 <adlerrivka66@gmail.com>
2022-01-02 11:47:05 +0200

    add classes

--- dotNet5782_0286_2346/DalObject/DalObject.cs
+++ dotNet5782_0286_2346/DalObject/DalObject.cs
@@ -8,14 +8,16 @@ using static Dal.DataSource;
 using DalApi;
 namespace Dal
 {
-    internal partial class DalObject : IDal
-    { 
+    sealed internal partial class DalObject : IDal
+    {
+        static readonly IDal instance = new DalObject();
+        public static IDal Instance { get => instance; }
         /// <summary>
         /// constructor, produce the data base
-        /// </summary>
-        
-        public DalObject()
+        /// </summary>        
+        DalObject()
         {
+
             DataSource.Initialize();
         }
 
