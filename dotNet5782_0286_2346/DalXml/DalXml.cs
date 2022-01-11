eed76722742e53c8bcecf2964f788a0d3c2973c8
rivkaadler1 <adlerrivka66@gmail.com>
2022-01-11 20:38:10 +0200

    dalXml functions and add xml file

--- dotNet5782_0286_2346/DalXml/DalXml.cs
+++ dotNet5782_0286_2346/DalXml/DalXml.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using DalApi;
 using DO;
 
@@ -21,6 +22,7 @@ namespace Dal
         string customersPath = @"CustomersXml.xml"; //XMLSerializer
         string parcelsPath = @"ParcelsXml.xml"; //XMLSerializer
         string stationsPath = @"StationsXml.xml"; //XMLSerializer
+        private readonly string configPath = "config.xml";
 
         public void AddStation(Station s)
         {
@@ -46,6 +48,15 @@ namespace Dal
         {
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// the function check an ID
+        /// </summary>
+        /// <param name="id">ID of drone</param:>
+        /// <returns>true if the id exists in the list otherwise it returns false </returns>
+        private bool checkStation(List<Station> lst, int id)
+        {
+            return lst.Exists(station => station.Id == id);
+        }
 
         public void AddCustomer(Customer c)
         {
@@ -54,12 +65,14 @@ namespace Dal
 
         public Customer GetCustomer(int id)
         {
-            List<Customer> customersList = XMLTools.LoadListFromXMLSerializer<Customer>(customersPath);
-            Customer c = customersList.Find(cust => cust.Id == id);
-            if (CheckCustomer(customersList,id))
-                return c; 
-            else
-                throw new IdNotFoundException(id, "customer");
+            //    List<Customer> customersList = XMLTools.LoadListFromXMLSerializer<Customer>(customersPath);
+            //    Customer c = customersList.Find(cust => cust.Id == id);
+            //    if (CheckCustomer(customersList,id))
+            //        return c; 
+            //    else
+            //        throw new IdNotFoundException(id, "customer");
+
+            throw new NotImplementedException();
         }
 
         public IEnumerable<Customer> GetListOfCustomers()
@@ -79,42 +92,108 @@ namespace Dal
         /// <returns>true if the id exists in the list otherwise it returns false </returns>
         private bool CheckCustomer(List<Customer>lst ,int id)
         {
-            return lst.Exists(cust => cust.Id == id);
+            //return lst.Exists(cust => cust.Id == id);
+
+            throw new NotImplementedException();
         }
 
         public void AddDrone(Drone d)
         {
-            throw new NotImplementedException();
+           List<Drone> dronesList = XMLTools.LoadListFromXMLSerializer<Drone>(dronesPath);
+           if (checkDrone(dronesList,d.Id))
+                throw new ExistIdException(d.Id, "drone");
+            dronesList.Add(d);
+            XMLTools.SaveListToXMLSerializer(dronesList, dronesPath);
         }
 
-        public void SendDroneToCharge(int id, int id2)
+        public void SendDroneToCharge(int idDrone, int idStation)
         {
-            throw new NotImplementedException();
+            DroneCharge dc = new DroneCharge();
+            List<Drone> dronesList = XMLTools.LoadListFromXMLSerializer<Drone>(dronesPath);
+            List<Station> stationsList = XMLTools.LoadListFromXMLSerializer<Station>(stationsPath);
+            List<DroneCharge> droneChargesList = XMLTools.LoadListFromXMLSerializer<DroneCharge>(droneChargesPath);
+            if (!checkDrone(dronesList,idDrone))
+                throw new DO.IdNotFoundException(idDrone, "drone");
+            if (!checkStation(stationsList,idStation))
+                throw new DO.IdNotFoundException(idStation, "station");
+            dc.DroneId = idDrone;
+            dc.StationId = idStation;
+            dc.StartOfCharging = DateTime.Now;
+            droneChargesList.Add(dc);
+            XMLTools.SaveListToXMLSerializer(droneChargesList, droneChargesPath);
+            Station s = stationsList.Find(stat => stat.Id == idStation);
+            s.ChargeSlots--;
+            stationsList.RemoveAll(stat => stat.Id == idStation);
+            stationsList.Add(s);
+            XMLTools.SaveListToXMLSerializer(stationsList, stationsPath);
         }
 
         public void ReleaseDroneFromCharge(int id)
         {
-            throw new NotImplementedException();
+            List<Drone> dronesList = XMLTools.LoadListFromXMLSerializer<Drone>(dronesPath);
+            List<Station> stationsList = XMLTools.LoadListFromXMLSerializer<Station>(stationsPath);
+            List<DroneCharge> droneChargesList = XMLTools.LoadListFromXMLSerializer<DroneCharge>(droneChargesPath);
+            if (!checkDrone(dronesList,id))
+                throw new DO.IdNotFoundException(id, "drone");
+            Station s;
+            foreach (DroneCharge charger in droneChargesList)
+            {
+                if (charger.DroneId == id)
+                {
+                    foreach (Station station in stationsList)
+                    {
+                        if (charger.StationId == station.Id)
+                        {
+                            s = station;
+                            s.ChargeSlots++;
+                            stationsList.Add(s);
+                            stationsList.Remove(station);
+                            droneChargesList.Remove(charger);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            XMLTools.SaveListToXMLSerializer(stationsList, stationsPath);
+            XMLTools.SaveListToXMLSerializer(droneChargesList, droneChargesPath);
         }
 
         public Drone GetDrone(int id)
         {
-            throw new NotImplementedException();
+            List<Drone> dronesList = XMLTools.LoadListFromXMLSerializer<Drone>(dronesPath);
+            Drone drone = dronesList.Find(drone => drone.Id == id);
+            if (checkDrone(dronesList, id))
+                return drone;
+            else
+                throw new IdNotFoundException(id, "drone");
         }
 
         public IEnumerable<Drone> GetListOfDrones()
         {
-            throw new NotImplementedException();
+            List<Drone> dronesList = XMLTools.LoadListFromXMLSerializer<Drone>(dronesPath);
+            return from drone in dronesList
+                   select drone; ;
         }
 
         public double[] GetDronePowerConsumption()
         {
-            throw new NotImplementedException();
+            return XMLTools.LoadListFromXMLElement(configPath).Element("BatteryUsages").Elements()
+                .Select(e => Convert.ToDouble(e.Value)).ToArray();
         }
 
         public void DeleteDrone(int id)
         {
             throw new NotImplementedException();
+        }
+        /// <summary>
+        /// the function check an ID
+        /// </summary>
+        /// <param name="id">ID of drone</param:>
+        /// <returns>true if the id exists in the list otherwise it returns false </returns>
+        private bool checkDrone(List<Drone> lst, int id)
+        {
+            return lst.Exists(drone => drone.Id == id);
         }
 
         public IEnumerable<DroneCharge> GetListOfBusyChargeSlots()
